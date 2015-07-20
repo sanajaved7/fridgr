55dca1b3f0bf9ad8536a50e5f28214b9e17f9875
Sana Javed <sanajaved7@gmail.com>
2015-07-20 00:02:10 -0400

    Added database functions and gitignore file

--- db.py
+++ db.py
@@ -1,15 +1,29 @@
 import sqlite3
 
+''' Starting database '''
 con = sqlite3.connect('fridge.db')
-
 cur = con.cursor()
-
 cur.execute('SELECT SQLITE_VERSION()')
-
 data = cur.fetchone()
-
 print(data)
 
-cur.execute('''CREATE TABLE items (name varchar(100) PRIMARY KEY, fridge Boolean, grocery Boolean, last_updated date, quantity int)''')
+
+def start_db():
+    cur.execute('''CREATE TABLE items (name varchar(100) PRIMARY KEY, fridge Boolean, grocery Boolean, last_updated date, quantity int)''')
+
+
+def add_item(name, fridge, grocery, date, quantity):
+    cur.execute("INSERT INTO items VALUES (?, ?, ?, ?, ?)",(name, fridge, grocery, date, quantity))
+    con.commit()
+
+
+def get_item():
+    cur.execute('SELECT * FROM items')
+    items = cur.fetchall()
+    print(items)
+
+add_item(name='apples', fridge=True, grocery=False, date='2015-07-20', quantity=5)
+
+get_item()
 
 con.close()
