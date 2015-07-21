7dcdcfa96f58eaf51317c73dcd9877530e360e47
Sana Javed <sanajaved7@gmail.com>
2015-07-20 21:24:51 -0400

    Added additional database functions

--- db.py
+++ db.py
@@ -17,13 +17,32 @@ def add_item(name, fridge, grocery, date, quantity):
     con.commit()
 
 
-def get_item():
+def get_all_items():
     cur.execute('SELECT * FROM items')
     items = cur.fetchall()
     print(items)
 
-add_item(name='apples', fridge=True, grocery=False, date='2015-07-20', quantity=5)
+def get_fridge_items():
+    cur.execute('SELECT * FROM items WHERE fridge = TRUE')
+    fridge_items = cur.fetchall()
+    print(fridge_items)
 
-get_item()
+def get_grocery_items():
+    cur.execute('SELECT * FROM items WHERE grocery = TRUE')
+    grocery_items = cur.fetchall()
+    print(grocery_items)
+
+def update_quantity(name, new_quantity):
+    cur.execute('UPDATE items SET quantity=? WHERE name = ?', (name, new_quantity))
+    con.commit()
+
+def delete_item(name):
+    cur.execute('DELETE FROM items WHERE name = ?', name)
+    con.commit()
+
+
+# add_item(name='apples', fridge=True, grocery=False, date='2015-07-20', quantity=5)
+
+get_all_items()
 
 con.close()
