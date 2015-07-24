82d3bfe16f01fc4002017beeab89b453ec25da7e
Sana Javed <sanajaved7@gmail.com>
2015-07-23 23:53:44 -0400

    Made flake8 updates

--- api.py
+++ api.py
@@ -5,9 +5,11 @@ our_fridge = Items()
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello():
     return "Hello World!"
+
 
 @app.route("/fridge")
 def fridge():
@@ -17,10 +19,12 @@ def fridge():
         fridge_list.append({'name': item[0], 'fridge': item[1], 'grocery': item[2], 'date': item[3], 'quantity': item[4]})
     return jsonify({"fridge": fridge_list})
 
+
 @app.route("/fridge/<name>")
 def fridge_item(name):
     item = our_fridge.get_one_item(name)
     return jsonify({'name': item[0], 'fridge': item[1], 'grocery': item[2], 'date': item[3], 'quantity': item[4]})
+
 
 @app.route("/grocery")
 def grocery():
