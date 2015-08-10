3f646daea132f99539717d82ab569fa85e1b563c
Sana Javed <sanajaved7@gmail.com>
2015-08-09 21:13:04 -0400

    Added routes to delete and create new items

--- api.py
+++ api.py
@@ -1,39 +1,52 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from db import Items
+import datetime
 our_fridge = Items()
 
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=['GET'])
 def hello():
-    return "Hello World!"
+    return "Hello and welcome to your personal fridge app! :D"
 
-
-@app.route("/fridge")
+@app.route("/fridge", methods=['GET', 'POST'])
 def fridge():
     """Returns all the items in the fridge"""
-    fridge_list = []
-    for row in our_fridge.get_fridge_items():
-        fridge_list.append(our_fridge.row_to_dict(row))
-    return jsonify({"fridge": fridge_list})
+    if request.method == 'GET':
+        fridge_list = []
+        for row in our_fridge.get_fridge_items():
+            fridge_list.append(our_fridge.row_to_dict(row))
+        return jsonify({"fridge": fridge_list})
+    elif request.method = 'POST':
+        name = request.form['name'],
+        quantity = request.form['quantity']
+        our_fridge.add_item(name=name, fridge=1, grocery=0, date=datetime.date.today(), quantity=quantity)
 
 
-@app.route("/fridge/<name>")
-def fridge_item(name):
-    """Returns one item info from the fridge"""
-    row = our_fridge.get_one_item(name)
-    return jsonify(our_fridge.row_to_dict(row))
-
-
-@app.route("/grocery")
+@app.route("/grocery", methods=['GET', 'POST'])
 def grocery():
     """Returns all items on our grocery list"""
-    grocery_list = []
-    for row in our_fridge.get_grocery_items():
-        grocery_list.append(our_fridge.row_to_dict(row))
-    return jsonify({"grocery": grocery_list})
+    if request.method == 'GET':
+        grocery_list = []
+        for row in our_fridge.get_grocery_items():
+            grocery_list.append(our_fridge.row_to_dict(row))
+        return jsonify({"grocery": grocery_list})
+    elif request.method == 'POST':
+        name = request.form['name'],
+        quantity = request.form['quantity']
+        our_fridge.add_item(name=name, fridge=0, grocery=1, date=datetime.date.today(), quantity=quantity)
+
+
+@app.route("/items/<name>", methods=['GET', 'DELETE'])
+def fridge_item(name):
+    """Returns one item info from the fridge"""
+    if request.method == 'GET':
+        row = our_fridge.get_one_item(name)
+        return jsonify(our_fridge.row_to_dict(row))
+    elif request.method == 'DELETE':
+        our_fridge.delete_item(name=name)
 
 @app.route("")
 
