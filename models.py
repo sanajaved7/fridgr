14ad2812d620674e051e630a0b52fe5f00bb7fb6
Sana Javed <sanajaved7@gmail.com>
2015-08-24 23:07:01 -0400

    Started converting database to use SQLAlchemy ORM

---
+++ models.py
@@ -0,0 +1,54 @@
+from views import db
+from datetime import datetime
+
+class Family(db.Model):
+    __tablename__ = "family"
+
+    family_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    members = db.relationship("User", backref="family")
+    fridge = db.relationship("Fridge", uselist=False, backref="family")
+
+
+class User(db.Model):
+    __tablename__ = "users"
+
+    user_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    password = db.Column(db.String, nullable=False)
+    family_id = db.Column(db.Integer, ForeignKey('family.family_id'))
+
+
+
+class Items(db.Model):
+    __tablename__ = "items"
+
+    item_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+    date_bought = db.Column(db.Date, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
+
+    def __init__(self, name, quantity, date_bought):
+        self.name = name
+        self.quantity = quantity
+        self.date_bought = date_bought
+
+
+class Fridge(db.Model):
+    __tablename__ = "fridge"
+
+    fridge_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False, unique=True)
+    family_id = db.Column(db.Integer, ForeignKey('family.family_id'))
+
+
+
+class Grocery(db.Model):
+    __tablename__ = "grocery"
+
+    grocery_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False, unique=True)
+
+    def __init__(self, name):
+        self.name = name
---
+++ views.py
@@ -0,0 +1,63 @@
+from flask import Flask, jsonify, request
+from models import Items
+import datetime
+from flask.ext.sqlalchemy import SQLAlchemy
+our_fridge = Items()
+
+
+app = Flask(__name__)
+db = SQLAlchemy(app)
+
+
+@app.route("/", methods=['GET'])
+def index():
+    return app.send_static_file('index.html')
+
+# @app.route("/fridge", methods=['GET', 'POST'])
+# def fridge():
+#     """Returns all the items in the fridge"""
+#     if request.method == 'GET':
+#         fridge_list = []
+#         for row in our_fridge.get_fridge_items():
+#             fridge_list.append(our_fridge.row_to_dict(row))
+#         return jsonify({"fridge": fridge_list})
+#     elif request.method == 'POST':
+#         name = request.form['name'],
+#         quantity = request.form['quantity']
+#         our_fridge.add_item(name=name, fridge=1, grocery=0, date=datetime.date.today(), quantity=quantity)
+
+
+# @app.route("/grocery", methods=['GET', 'POST'])
+# def grocery():
+#     """Returns all items on our grocery list"""
+#     if request.method == 'GET':
+#         grocery_list = []
+#         for row in our_fridge.get_grocery_items():
+#             grocery_list.append(our_fridge.row_to_dict(row))
+#         return jsonify({"grocery": grocery_list})
+#     elif request.method == 'POST':
+#         name = request.form['name'],
+#         quantity = request.form['quantity']
+#         our_fridge.add_item(name=name, fridge=0, grocery=1, date=datetime.date.today(), quantity=quantity)
+
+
+# @app.route("/items/<name>", methods=['GET', 'DELETE', 'POST'])
+# def fridge_item(name):
+#     """Returns one item info from the fridge"""
+#     if request.method == 'GET':
+#         row = our_fridge.get_one_item(name)
+#         return jsonify(our_fridge.row_to_dict(row))
+#     elif request.method == 'DELETE':
+#         our_fridge.delete_item(name=name)
+#     elif request.method == 'POST':
+#         fridge = request.form['fridge']
+#         grocery = request.form['grocery']
+#         our_fridge.update_location(name=name, fridge=fridge, grocery=grocery)
+
+# @app.route('/<path:path>')
+# def static_proxy(path):
+#     """send_static_file will guess the correct MIME type"""
+#     return app.send_static_file(path)
+
+if __name__ == "__main__":
+    app.run()
