b2bdf736b243375cc4eeac458dc1ff81437a4b60
ramirezg <gabriel.ramirez@gsa.gov>
2015-11-23 22:31:18 -0500

    adding tests to the flask models

--- test_database.py
+++ test_database.py
@@ -6,7 +6,7 @@ from views import app, db
 
 class MyTest(TestCase):
 
-    SQLALCHEMY_DATABASE_URI = "sqlite://"
+    SQLALCHEMY_DATABASE_URI = "sqlite://test.db"
     TESTING = True
 
     def create_app(self):
@@ -19,30 +19,25 @@ class MyTest(TestCase):
         user = User(name='user1', email="test@gmail.com")
         user2 = User(name='user2',  email="test2@gmail.com")
         user3 = User(name='user3',  email="test3@gmail.com")
-        db.session.add(user)
-        db.session.add(user2)
-        db.session.add(user3)
+        db.session.add_all([user, user2, user3])
         # Adding Families
         family = Family(name='Family-name-1', members=[user, user2])
         family2 = Family(name='Family-name-2', members=[user, user3])
-        db.session.add(family)
-        db.session.add(family2)
+        db.session.add_all([family, family2, user3])
         # Adding Fridges
         fridge = Fridge()
         fridge2 = Fridge()
-        db.session.add(fridge)
-        db.session.add(fridge2)
+        db.session.add_all([fridge, fridge2])
         family.fridge = fridge
         family2.fridge = fridge2
-        db.session.add(fridge)
-        db.session.add(fridge2)
         # Adding Grocery Lists
         grocery_list = GroceryList(name="listname1")
         grocery_list2 = GroceryList(name="listname2")
-        db.session.add(grocery_list)
-        db.session.add(grocery_list2)
+        grocery_list3 = GroceryList(name="listname3")
+        db.session.add_all([grocery_list, grocery_list2, grocery_list3])
         family.grocery_lists.append(grocery_list)
-        family2.grocery_lists.append(grocery_list2)
+        family.grocery_lists.append(grocery_list2)
+        family2.grocery_lists.append(grocery_list3)
         db.session.commit()
         # Adding items
         item1 = Item(name="item1", family_pk=family.pk)
@@ -50,18 +45,14 @@ class MyTest(TestCase):
         item3 = Item(name="item3", family_pk=family.pk)
         item4 = Item(name="item4", family_pk=family2.pk)
         item5 = Item(name="item5", family_pk=family2.pk)
-        db.session.add(item1)
-        db.session.add(item2)
-        db.session.add(item3)
-        db.session.add(item4)
-        db.session.add(item5)
+        db.session.add_all([item1, item2, item3, item4, item5])
         # Adding items to fridge
         fridge.items.append(item1)
         fridge2.items.append(item2)
         # Adding items to grocery lists
         grocery_list.items.append(item3)
-        grocery_list2.items.append(item4)
-        grocery_list2.items.append(item5)
+        grocery_list3.items.append(item4)
+        grocery_list3.items.append(item5)
         db.session.commit()
 
 
@@ -91,7 +82,41 @@ class MyTest(TestCase):
         user = db.session.query(User).filter(User.name=="user1").one()
         self.assertEqual(len(user.family), 2)
 
-    def test_
+    def test_can_access_fridge_via_family(self):
+        """ Check that the fridge can be access via the family model """
+        family = db.session.query(Family).filter(Family.name=="Family-name-1").one()
+        fridge = db.session.query(Fridge).filter(Fridge.family_pk==family.pk).one()
+        self.assertEqual(family.fridge, fridge)
+
+    def test_grocery_list_access_via_family(self):
+        """ Check that the grocery_list can be access via the family model """
+        family = db.session.query(Family).filter(Family.name=="Family-name-1").one()
+        g_lists = db.session.query(Fridge).filter(GroceryList.family_pk==family.pk).all()
+        self.assertEqual(len(family.grocery_lists), 2)
+
+    def test_items_in_fridge(self):
+        """ Check that the fridge contains items """
+        family = db.session.query(Family).filter(Family.name=="Family-name-2").one()
+        self.assertEqual(len(family.fridge.items), 1)
+
+    def test_items_in_grocery_list(self):
+        """ Check that the fridge contains items """
+        family = db.session.query(Family).filter(Family.name=="Family-name-2").one()
+        self.assertEqual(len(family.grocery_lists[0].items), 2)
+
+    def test_that_item_can_only_be_in_fridge_or_glist(self):
+        """ Test that an item can only be either in the fridge or a grocery list """
+        family = db.session.query(Family).filter(Family.name=="Family-name-2").one()
+        # Check number of items in fridge and glist
+        self.assertEqual(len(family.fridge.items), 1)
+        self.assertEqual(len(family.grocery_lists[0].items), 2)
+        # Switch the item to glist
+        family.fridge.items.append(family.grocery_lists[0].items[0])
+        db.session.commit()
+        # Check that the item moved over
+        self.assertEqual(len(family.fridge.items), 2)
+        self.assertEqual(len(family.grocery_lists[0].items), 1)
+
 
 if __name__ == "__main__":
     unittest.main()
